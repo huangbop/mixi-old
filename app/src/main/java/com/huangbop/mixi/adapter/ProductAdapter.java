ee0b217fadd0a55c64e5d67575f13878d604b0d0
Huang Bo <huangbop@gmail.com>
2015-06-08 07:53:13 +0800

    List products

---
+++ app/src/main/java/com/huangbop/mixi/adapter/ProductAdapter.java
@@ -0,0 +1,67 @@
+package com.huangbop.mixi.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.huangbop.mixi.R;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/6/7.
+ */
+public class ProductAdapter extends BaseAdapter {
+
+  LayoutInflater inflater;
+  List<String> products;
+
+  public ProductAdapter(Context context, List<String> products) {
+    inflater = LayoutInflater.from(context);
+    this.products = products;
+  }
+
+
+  @Override
+  public int getCount() {
+    return products.size();
+  }
+
+  @Override
+  public Object getItem(int position) {
+    return null;
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return 0;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    ViewHolder holder;
+
+    if (convertView == null) {
+      convertView = inflater.inflate(R.layout.listview_product, null);
+      holder = new ViewHolder();
+      holder.product_image = (ImageView) convertView.findViewById(R.id.product_image);
+      holder.product_information = (TextView) convertView.findViewById(R.id.product_information);
+      convertView.setTag(holder);
+    } else {
+      holder = (ViewHolder) convertView.getTag();
+    }
+
+    return convertView;
+  }
+
+  class ViewHolder {
+    ImageView product_image;
+    TextView product_information;
+  }
+}
--- app/src/main/java/com/huangbop/mixi/fragment/ProductFragment.java
+++ app/src/main/java/com/huangbop/mixi/fragment/ProductFragment.java
@@ -1,19 +1,35 @@
 package com.huangbop.mixi.fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.huangbop.mixi.R;
+import com.huangbop.mixi.adapter.ProductAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProductFragment extends Fragment {
 
+  @InjectView(R.id.productList)
+  ListView productList;
+
+  Context context;
+  ProductAdapter productAdapter;
+
+  ArrayList<String> products = new ArrayList<>();
 
   public ProductFragment() {
     // Required empty public constructor
@@ -24,8 +40,22 @@ public class ProductFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_product, container, false);
+    View currentView = inflater.inflate(R.layout.fragment_product, container, false);
+    ButterKnife.inject(this, currentView);
+
+    getProducts();
+
+    context = getActivity();
+    productAdapter = new ProductAdapter(context, products);
+    productList.setAdapter(productAdapter);
+
+    return currentView;
   }
 
+  private void getProducts() {
+    for (int i = 0; i < 10; i++) {
+      products.add("xxx");
+    }
+  }
 
 }
