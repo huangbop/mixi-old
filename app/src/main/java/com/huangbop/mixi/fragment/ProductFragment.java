d4d7bf018f6ab360c02161ea818f5b290149ac2c
Huang Bo <huangbop@gmail.com>
2015-06-08 16:29:11 +0800

    Rounded list item

--- app/src/main/java/com/huangbop/mixi/fragment/ProductFragment.java
+++ app/src/main/java/com/huangbop/mixi/fragment/ProductFragment.java
@@ -53,7 +53,7 @@ public class ProductFragment extends Fragment {
   }
 
   private void getProducts() {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 100; i++) {
       products.add("xxx");
     }
   }
