bda527c8bd8fa00bf2b76acc79ac085ed73726dd
Huang Bo <huangbop@gmail.com>
2015-08-21 15:51:43 +0800

    Login

--- app/src/main/java/com/huangbop/mixi/fragment/OrderFragment.java
+++ app/src/main/java/com/huangbop/mixi/fragment/OrderFragment.java
@@ -2,46 +2,19 @@ package com.huangbop.mixi.fragment;
 
 
 import android.os.Bundle;
-
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.huangbop.mixi.R;
-import com.huangbop.mixi.api.gitapi;
-import com.huangbop.mixi.model.SnippetModel;
-import com.huangbop.mixi.model.gitmodel;
-import com.huangbop.mixi.network.SnippetApi;
-
-import java.util.List;
 
 import butterknife.ButterKnife;
-import butterknife.InjectView;
-import retrofit.Callback;
-import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class OrderFragment extends Fragment {
-
-  @InjectView(R.id.button)
-  Button click;
-  @InjectView(R.id.tv)
-  TextView tv;
-  @InjectView(R.id.edit)
-  EditText edit_user;
-  @InjectView(R.id.pb)
-  ProgressBar pbar;
-
-  String API = "https://api.github.com";
 
   public OrderFragment() {
     // Required empty public constructor
@@ -54,48 +27,6 @@ public class OrderFragment extends Fragment {
     // Inflate the layout for this fragment
     View currentView = inflater.inflate(R.layout.fragment_order, container, false);
     ButterKnife.inject(this, currentView);
-
-    click.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint("http://mixi.today/api/v1").build();
-        SnippetApi snippetApi = restAdapter.create(SnippetApi.class);
-        snippetApi.getData(new Callback<List<SnippetModel>>() {
-          @Override
-          public void success(List<SnippetModel> snippetModels, Response response) {
-            int i = 0;
-          }
-
-          @Override
-          public void failure(RetrofitError error) {
-            int i = 0;
-          }
-        });
-      }
-    });
-
-//    click.setOnClickListener(new View.OnClickListener() {
-//      @Override
-//      public void onClick(View v) {
-//        String user = edit_user.getText().toString();
-//
-//        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(API).build();
-//        gitapi api = restAdapter.create(gitapi.class);
-//
-//        api.getFeed(user, new Callback<gitmodel>() {
-//          @Override
-//          public void success(gitmodel gitmodel, Response response) {
-//            Integer i = 0;
-//          }
-//
-//          @Override
-//          public void failure(RetrofitError error) {
-//            Integer i = 0;
-//          }
-//        });
-//
-//      }
-//    });
 
     return currentView;
   }
